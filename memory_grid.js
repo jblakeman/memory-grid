79eeafd88cd5859e3bb80f23b3c1b80aca7e3a9d
jblakeman <blakeman.john@gmail.com>
2016-02-23 13:52:39 -0500

    Hide populated cells after timeout, remove grid unnecessary array

--- memory_grid.js
+++ memory_grid.js
@@ -1,8 +1,9 @@
 var memory = {
     gridHeight: 6,
     gridWidth: 5,
-    gridArray: [],
     numGuesses: 0,
+    rCells: {},
+    hideAfterMs: 3000,
     grid: document.getElementById("grid"),
     gridRows: document.getElementsByClassName("row"),
     assignNumGuesses: function() {
@@ -17,33 +18,44 @@ var memory = {
         var rowElement, row, i, j;
         this.assignNumGuesses();
         for (i = 0; i < this.gridHeight; i++) {
-            row = [];
             rowElement = this.createNewElement("tr", "row");
             for (j = 0; j < this.gridWidth; j++) {
                 rowElement.appendChild(this.createNewElement("td", "cell"));
-                row.push(false);
             }
             this.grid.appendChild(rowElement);
-            this.gridArray.push(row);
         }
     },
     populateRandomGrid: function() {
-        var numAssigned = 0, randomCells = {}, r1, r2;
+        var numAssigned = 0, r1, r2;
+        this.rCells = {};
         while(numAssigned < this.numGuesses) {
             r1 = Math.floor(Math.random() * this.gridHeight);
-            if (!(r1 in randomCells)) {
-                randomCells[r1] = {};
+            if (!(r1 in this.rCells)) {
+                this.rCells[r1] = {};
             }
-            if (Object.keys(randomCells[r1]).length < this.gridWidth) {
+            if (Object.keys(this.rCells[r1]).length < this.gridWidth) {
                 r2 = Math.floor(Math.random() * this.gridWidth);
-                if (!(r2 in randomCells[r1])) {
-                    randomCells[r1][r2] = true;
-                    this.gridArray[r1][r2] = randomCells[r1][r2];
+                if (!(r2 in this.rCells[r1])) {
+                    this.rCells[r1][r2] = true;
+                    var cellElem = this.gridRows[r1].childNodes;
+                    cellElem[r2].style.backgroundColor = "black";
                     numAssigned++;
                 }
             }
         }
+    },
+    hideFilled: function() {
+        setTimeout(function() {
+            var cells, key1, key2;
+            for (key1 in this.rCells) {
+                cells = this.gridRows[key1].childNodes;
+                for (key2 in this.rCells[key1]) {
+                    cells[key2].style.backgroundColor = "white";
+                }
+            }
+        }.bind(this), this.hideAfterMs);
     }
 };
 memory.createGrid();
 memory.populateRandomGrid();
+memory.hideFilled();
