91aa2fd3d238dcedbfeb5924b372af7602c6f386
jblakeman <blakeman.john@gmail.com>
2016-02-23 23:38:05 -0500

    Fix grid population/hide errors

--- memory_grid.js
+++ memory_grid.js
@@ -4,8 +4,7 @@ var memory = {
     numGuesses: 0,
     rCells: {},
     hideAfterMs: 3000,
-    grid: document.getElementById("grid"),
-    gridRows: document.getElementsByClassName("row"),
+    grid: [],
     assignNumGuesses: function() {
         this.numGuesses = Math.floor((this.gridHeight*this.gridWidth)/3)+1;
     },
@@ -14,20 +13,24 @@ var memory = {
         if (className) newElement.className = className;
         return newElement;
     },
-    changeBackgroundColor: function(event, color, prevent) {
+    changeBackgroundColor: function(event, color) {
         event.target.style.backgroundColor = color;
-        if (prevent) event.prevenDefault();
+        event.preventDefault();
     },
     createGrid: function() {
-        var rowElement, row, cell, i, j;
+        var row, rowElement, cell, i, j;
+        var table = document.getElementById("grid");
         this.assignNumGuesses();
-        for (i = 0; i < this.gridHeight; i++) {
+		for (i = 0; i < this.gridHeight; i++) {
             rowElement = this.createNewElement("tr", "row");
+            row = [];
             for (j = 0; j < this.gridWidth; j++) {
                 cell = this.createNewElement("td", "cell");
+                row.push(cell);
                 rowElement.appendChild(cell);
             }
-            this.grid.appendChild(rowElement);
+            this.grid.push(row);
+            table.appendChild(rowElement);
         }
     },
     populateRandomGrid: function() {
@@ -42,32 +45,28 @@ var memory = {
                 r2 = Math.floor(Math.random() * this.gridWidth);
                 if (!(r2 in this.rCells[r1])) {
                     this.rCells[r1][r2] = true;
-                    var cellElem = this.gridRows[r1].childNodes;
-                    cellElem[r2].style.backgroundColor = "black";
+                    this.grid[r1][r2].style.backgroundColor = "black";
                     numAssigned++;
                 }
             }
         }
+        console.log(self.rCells);
     },
     hideFilled: function() {
         self = this;
         setTimeout(function() {
-            Object.getOwnPropertyNames(self.gridRows).forEach(function(key1) {
-                var cells = self.gridRows[key1].childNodes;
-                Object.getOwnPropertyNames(cells).forEach(
-                    function(key2) {
-                        var prevent = false, color = "red";
-                        if (key2 in self.rCells[key1]) {
-                            color = "green";
-                            prevent = true;
-                        }
-                        cells[key2].addEventListener("click", function(event) {
-                            self.numGuesses++;
-                            self.changeBackgroundColor(event, color, prevent);
-                        });
-                        cells[key2].style.backgroundColor = "white";
+            self.grid.forEach(function(row, i1) {
+                row.forEach(function(cell, i2) {
+                    var color = "red";
+                    if (i1 in self.rCells && i2 in self.rCells[i1]) {
+                        color = "green";
+                    }
+                    cell.addEventListener("click", function(event) {
+                        self.numGuesses++;
+                        self.changeBackgroundColor(event, color);
                     });
-
+                    cell.style.backgroundColor = "white";
+                });
             });
         }, self.hideAfterMs);
     }
